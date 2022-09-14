c6aa17b02c5cc39f88262e84fbfa101115eb52ba
Marco <contact@marcobartsch.me>
2022-09-14 12:03:59 -0700

    Commit from TeleportHQ

--- src/components/need-help-with-a-project.js
+++ src/components/need-help-with-a-project.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import PropTypes from 'prop-types'
 
+import MainCTAButton from './main-c-t-a-button'
 import './need-help-with-a-project.css'
 
 const NeedHelpWithAProject = (props) => {
@@ -18,6 +19,23 @@ const NeedHelpWithAProject = (props) => {
           <span className="need-help-with-a-project-text2">
             {props.option1_details}
           </span>
+          <img
+            src={props.image_src}
+            alt={props.image_alt}
+            className="need-help-with-a-project-image"
+          />
+          <a
+            href="https://shop.glowingkitty.com/products/ask-a-mentor"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="need-help-with-a-project-link"
+          >
+            <MainCTAButton
+              rootClassName="main-c-t-a-button-root-class-name1"
+              header_cta="Book an appointment"
+              className="need-help-with-a-project-component"
+            ></MainCTAButton>
+          </a>
         </div>
         <div className="need-help-with-a-project-container3">
           <span className="need-help-with-a-project-text3">
@@ -26,6 +44,23 @@ const NeedHelpWithAProject = (props) => {
           <span className="need-help-with-a-project-text4">
             {props.option2_details}
           </span>
+          <img
+            src={props.image_src1}
+            alt={props.image_alt1}
+            className="need-help-with-a-project-image1"
+          />
+          <a
+            href="http://discord.gg/Gq8xmN9tnD"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="need-help-with-a-project-link1"
+          >
+            <MainCTAButton
+              rootClassName="main-c-t-a-button-root-class-name2"
+              header_cta="Join us on Discord"
+              className="need-help-with-a-project-component1"
+            ></MainCTAButton>
+          </a>
         </div>
       </div>
     </div>
@@ -39,6 +74,10 @@ NeedHelpWithAProject.defaultProps = {
   option2: 'Option 2:',
   option1_details: 'Get 1 to 1 help from glowingkitty!',
   option1: 'Option 1:',
+  image_src: '/playground_assets/twopeople.svg',
+  image_alt: 'image',
+  image_src1: '/playground_assets/discord.svg',
+  image_alt1: 'image',
 }
 
 NeedHelpWithAProject.propTypes = {
@@ -48,6 +87,10 @@ NeedHelpWithAProject.propTypes = {
   option2: PropTypes.string,
   option1_details: PropTypes.string,
   option1: PropTypes.string,
+  image_src: PropTypes.string,
+  image_alt: PropTypes.string,
+  image_src1: PropTypes.string,
+  image_alt1: PropTypes.string,
 }
 
 export default NeedHelpWithAProject
